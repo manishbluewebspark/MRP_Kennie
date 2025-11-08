e79abe518886eaed6d9a429ddc692fb97077ca7d
Bluewebspark <bluewebspark@Bluewebsparks-MacBook-Air.local>
2025-11-08 11:55:17 +0530

    update prod env in ENVOIRTMENR CONFIG file

--- frontend/src/configs/EnvironmentConfig.js
+++ frontend/src/configs/EnvironmentConfig.js
@@ -3,11 +3,11 @@ const dev = {
 };
 
 const prod = {
-  API_ENDPOINT_URL: "http://13.204.207.229/api"  // Live backend
+  API_ENDPOINT_URL: "http://13.204.207.229/api/api"  // Live backend
 };
 
 const test = {
-  API_ENDPOINT_URL: "http://13.204.207.229/api" // Staging backend
+  API_ENDPOINT_URL: "https://staging.yourdomain.com/api" // Staging backend
 };
 
 const getEnv = () => {
